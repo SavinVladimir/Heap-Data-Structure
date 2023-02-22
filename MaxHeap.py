552020f0e06e74576bbbc406267e4dc7cda0c6cc
Savin Vladimir <savinvladimircs@gmail.com>
2023-02-22 12:58:14 +0300

    Min/MaxHeap implementation Beta

---
+++ MaxHeap.py
@@ -0,0 +1,45 @@
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+        self.size = -1
+
+    def siftUp(self, index):
+        while index > 0:
+            parent = (index - 1) // 2
+            if self.heap[parent] >= self.heap[index]:
+                break
+            self.swap(parent, index)
+            index = parent
+
+    def siftDown(self, index):
+        while 2 * index + 1 < self.size:
+            left = 2 * index + 1
+            right = 2 * index + 2
+            j = left
+            if right < self.size and self.heap[right] > self.heap[left]:
+                j = right
+            if self.heap[index] >= self.heap[j]:
+                break
+            self.swap(index, j)
+            index = j
+
+    def swap(self, first: int, second: int):
+        self.heap[first], self.heap[second] = self.heap[second], self.heap[first]
+
+    def insert(self, k: int):
+        self.heap.append(k)
+        self.size += 1
+        self.siftUp(self.size)
+
+    def extract(self):
+        if self.size > 0:
+            self.heap[0] = self.heap[self.size]
+            self.siftDown(0)
+        del self.heap[self.size]
+        self.size -= 1
+
+    def maxValue(self):
+        return self.heap[0]
+
+    def array(self):
+        return self.heap
---
+++ MinHeap.py
@@ -0,0 +1,45 @@
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+        self.size = -1
+
+    def siftUp(self, index):
+        while index > 0:
+            parent = (index - 1) // 2
+            if self.heap[parent] <= self.heap[index]:
+                break
+            self.swap(parent, index)
+            index = parent
+
+    def siftDown(self, index):
+        while 2 * index + 1 < self.size:
+            left = 2 * index + 1
+            right = 2 * index + 2
+            j = left
+            if right < self.size and self.heap[right] < self.heap[left]:
+                j = right
+            if self.heap[index] <= self.heap[j]:
+                break
+            self.swap(index, j)
+            index = j
+
+    def swap(self, first: int, second: int):
+        self.heap[first], self.heap[second] = self.heap[second], self.heap[first]
+
+    def insert(self, k):
+        self.heap.append(k)
+        self.size += 1
+        self.siftUp(self.size)
+
+    def extract(self):
+        if self.size > 0:
+            self.heap[0] = self.heap[self.size]
+            self.siftDown(0)
+            del self.heap[self.size]
+            self.size -= 1
+
+    def min_value(self):
+        return self.heap[0]
+
+    def array(self):
+        return self.heap
